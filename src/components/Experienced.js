6dea7720b4bb0c92f645aa4e8e8dd35a90a9eea2
Olawande Akinmosin <samueldacoal@gmail.com>
2022-06-21 18:18:38 +0100

    update

--- src/components/Experienced.js
+++ src/components/Experienced.js
@@ -8,9 +8,9 @@ const Experienced = () => {
       <div className="d-flex row  flex-md-row content justify-content-between">
           <div className="col-md-4">
             <header className="text-center text-lg-start ">
-              <h2 className="title">Learn From Industries Best</h2>
+              <h4 className="title" style={{fontSize:"27px"}} >Learn From Industries Best</h4>
             </header>
-            <main className="mt-mid">
+            <main className="mt-3">
               <p className="text paragraph">
                 Gotocourse Instructors are experienced practitioners who work at world's most innovative firms, you are going to  learn the most in-demand skills relevant in today’s workplace.
               </p>
@@ -18,9 +18,9 @@ const Experienced = () => {
           </div>
           <div className="col-md-8">
             <div className="d-flex flex-wrap  justify-content-center px-lg-5 mt-5" style={{gap:"1rem"}}>
-            {icons.slice(5, 11).map(icon=>(
-                <div className="d-flex align-items-center" style={{width:"100px", height:"65px"}}>
-                <img src={icon} alt="" style={{width:"100px", height:"65px", maxWidth:"100%"}} />
+            {icons.slice(0, 6).map(icon=>(
+                <div className="d-flex align-items-center" style={{width:"100px", height:"50px"}}>
+                <i className="icon experienced_icon" style={{width:"100px", height:"50px"}}>{icon}</i>
                 </div>
             ))}
             </div>  
