95fafc1a5e11cf5c4f17a44e0db5f76dfa73061d
Olawande Akinmosin <samueldacoal@gmail.com>
2022-07-10 17:38:13 +0100

    update

--- src/pages/Teacher/index.jsx
+++ src/pages/Teacher/index.jsx
@@ -111,7 +111,7 @@ export const Home = () => {
                     <Grid container spacing={4}>
                         {
                             data.map(({icon: Icon, title, content}, i) => (
-                                <Grid item xs={12} sm={12} md={6} lg={4} key={i}>
+                                <Grid item xs={12} sm={12} md={6} lg={3} key={i}>
                                     <Paper elevation={1} className={clsx.paper}>
                                         <span>
                                             <Icon />
