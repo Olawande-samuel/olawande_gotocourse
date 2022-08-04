fcad2d3e03cc5bce1f12855c6e922899fe5f70ea
austin230196 <austinakamelu55@gmail.com>
2022-08-04 13:32:01 +0100

    Added requests table on the admin earning page

--- src/pages/Dashboard/Teachers/Earnings.jsx
+++ src/pages/Dashboard/Teachers/Earnings.jsx
@@ -1,8 +1,9 @@
-import { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import { Modal, Box } from "@mui/material";
+import { Modal, Box, Switch } from "@mui/material";
 import { FiFilter } from "react-icons/fi";
+import {HiOutlineFilter} from "react-icons/hi";
 
 
 import { Chart as ChartLogo } from "../../../images/components/svgs";
@@ -13,32 +14,8 @@ import { useLocalStorage } from "../../../hooks";
 import {KEY} from "../../../constants"
 import MyChart from "../../../components/Chart";
 
-const data = [
-  {
-    id: 1,
-    title: "Day",
-  },
-  {
-    id: 2,
-    title: "Week",
-  },
-  {
-    id: 3,
-    title: "1 month",
-  },
-  {
-    id: 4,
-    title: "3 months",
-  },
-  {
-    id: 5,
-    title: "6 months",
-  },
-  {
-    id: 6,
-    title: "1 year",
-  },
-];
+
+
 export default function Earnings() {
     const { getItem } = useLocalStorage();
     let userdata = getItem(KEY);
@@ -46,15 +23,30 @@ export default function Earnings() {
       generalState: { isMobile },
     } = useAuth();
     return (
-      <Teachers isMobile={isMobile} userdata={userdata}>
+      <Teachers isMobile={isMobile} userdata={userdata} style={{overflowY: "scroll"}}>
         <div className={clsx.teachers_profile}>
           <AllEarnings />
+        </div>
+        <div className={clsx.teachers_profile} style={{marginTop:20}}>
+          <Requests />
         </div>
       </Teachers>
     );
   }
   
   export function AllEarnings (){
+    const data = [
+      {
+        title: "Teaching Model",
+        type: "COHORT",
+        value: 0
+      },
+      {
+        title: "Teaching Model",
+        type: "COHORT",
+        value: 0
+      }
+    ]
     return (
       <>
       <div
@@ -72,9 +64,11 @@ export default function Earnings() {
           <div
             className="d-flex flex-wrap justify-content-center justify-content-md-start"
             style={{ gap: "1.5rem" }}
-          >
-            <EarningsCard title="Teaching Model" type="COHORT" value="0" />
-            <EarningsCard title="Per Course" type="Cybersecurity" value="0" />
+          >{
+            data.map(({title, type, value}, i) => (
+              <EarningsCard title={title} type={type} value={value} key={i} />
+            ))
+          }
             <EarningsCard total={true} value="0" />
           </div>
           <div  className="overflow-auto">
@@ -134,3 +128,209 @@ export default function Earnings() {
     );
   }
   
+
+
+
+const Requests = () => {
+  const [data, setData] = useState([
+    {
+      title: "Day", 
+      active: true
+    },
+    {
+      title: "Week", 
+      active: false
+    },
+    {
+      title: "1 month", 
+      active: false
+    },
+    {
+      title: "3 months", 
+      active: false
+    },
+    {
+      title: "6 months", 
+      active: false
+    },
+    {
+      title: "1 year", 
+      active: false
+    },
+  ])
+
+  const rowData = [
+    {
+      header: "No",
+      entries: [
+        {
+          id: 1,
+          title: "1."
+        },
+        {
+          id: 2,
+          title: "2."
+        },
+        {
+          id: 3,
+          title: "3."
+        },
+      ]
+    },
+    {
+      header: "Name of Teacher",
+      entries: [
+        {
+          id: 1,
+          title: "Peter Cole"
+        },
+        {
+          id: 2,
+          title: "James Jackson"
+        },
+        {
+          id: 3,
+          title: "Lucas mata"
+        },
+      ]
+    },
+    {
+      header: "Date",
+      entries: [
+        {
+          id: 1,
+          title: "02-04-12"
+        },
+        {
+          id: 2,
+          title: "02-02-12"
+        },
+        {
+          id: 3,
+          title: "04-31-24"
+        },
+      ]
+    },
+    {
+      header: "Account Details",
+      entries: [
+        {
+          id: 1,
+          title: "UBA-84458588"
+        },
+        {
+          id: 2,
+          title: "JPM-873747484"
+        },
+        {
+          id: 3,
+          title: "CITI-848487484"
+        },
+      ]
+    },
+    {
+      header: "Amount",
+      entries: [
+        {
+          id: 1,
+          title: "25%"
+        },
+        {
+          id: 2,
+          title: "50%"
+        },
+        {
+          id: 3,
+          title: "25% balance"
+        },
+      ]
+    },
+    {
+      header: "Approve Request",
+      entries: [
+        {
+          id: 1,
+          title: false
+        },
+        {
+          id: 2,
+          title: false
+        },
+        {
+          id: 3,
+          title: true
+        },
+      ]
+    }
+  ]
+
+  function toggleActiveHandler(e){
+    let title = e.target.textContent;
+    console.log(title);
+    setData(old => {
+      let copy = [...old];
+      return copy.map(c => {
+        if(c.title === title) {
+          c.active = true;
+        }else {
+          c.active = false;
+        }
+        return c;
+      })
+    })
+  }
+  return(
+    <div className={clsx.requests}>
+      <div className={clsx.requests_header}>
+        <h2>List of Requests</h2>
+        <button>Download CSV</button>
+      </div>
+      <div className={clsx.requests_meta}>
+        <span>
+          <HiOutlineFilter /> &nbsp; Filter by:
+        </span>
+        {
+          data.map(({active, title}, i) => (
+            <Tab toggleActiveHandler={toggleActiveHandler} key={i} active={active}>
+              {title}
+            </Tab>
+          ))
+        }
+      </div>
+      <RequestsTable rows={rowData} />
+    </div>
+  )
+}
+
+
+const Tab = ({children, active, toggleActiveHandler}) => {
+  return (
+    <div onClick={toggleActiveHandler} className={`${clsx.requests_tab} ${active ? 'in_active' : ''}`}>
+      {children}
+    </div>
+  )
+}
+
+
+const RequestsTable = ({rows}) => {
+  return (
+    <div className={clsx.requests_table}>
+      {
+        rows.map(({header, entries}, i) => (
+          <div key={i} className={clsx.requests_table_column}>
+            <h6>{header}</h6>
+            <ul>
+              {
+                entries.map((e, i) => (
+                  <li key={i}>
+                   {header === "Approve Request" ? <Switch checked={e.title} /> : e.title}
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
+        ))
+      }
+    </div>
+  )
+} 
