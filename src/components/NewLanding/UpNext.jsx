2fd165964002d132f0eba235aee101c7c278d546
Olawande Akinmosin <samueldacoal@gmail.com>
2022-12-23 19:06:45 +0100

    updates

--- src/components/NewLanding/UpNext.jsx
+++ src/components/NewLanding/UpNext.jsx
@@ -26,7 +26,7 @@ import { Box, Popover } from '@mui/material';
 const Grid = styled.div`
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(min(200px, 230px), 230px));
-    grid-auto-rows: 352px;
+    grid-auto-rows: 392px;
     /* overflow: hidden; */
     gap: 1.5rem;
     justify-content:space-around;
@@ -52,6 +52,7 @@ const UpCoursesCard = styled.div`
     /* border: 2.2648px solid rgba(0, 114, 239, 0.5);
     padding: clamp(0.03125rem, -0.2813rem + 1.5625vw, 1.125rem);
     border-radius: 8px; */
+    height: 390px;
     display: flex;
     flex-direction:column;
     box-shadow: -10px 159px 64px rgba(0, 0, 0, 0.01), -6px 90px 54px rgba(0, 0, 0, 0.05), -3px 40px 40px rgba(0, 0, 0, 0.09), -1px 10px 22px rgba(0, 0, 0, 0.1), 0px 0px 0px rgba(0, 0, 0, 0.1);
@@ -196,9 +197,9 @@ export function TabsComp() {
         <Swiper
           // install Swiper modules
           modules={[Navigation, Autoplay, Pagination, Scrollbar, A11y]}
-          loop={false}
-          speed={1500}
-          autoplay={{ delay: 2500 }}
+          loop={true}
+          speed={2500}
+          autoplay={{ delay: 2400 }}
           spaceBetween={0}
           slidesPerView={1}
           // navigation
