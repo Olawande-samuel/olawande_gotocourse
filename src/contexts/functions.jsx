42e6c6649a6d454ffb22233112cc1d548e5c3801
Olawande Akinmosin <samueldacoal@gmail.com>
2023-01-02 15:04:16 +0100

    updates

--- src/contexts/functions.jsx
+++ src/contexts/functions.jsx
@@ -2528,6 +2528,34 @@ export const adminFunctions = {
         }
     },
 
+    fetchLeads: async function (token) {
+        try {
+            const res = await axios.get(`${baseURL}/programs/all`,
+                {
+                    headers: {
+                        "Authorization": `Bearer ${token}`,
+                        "Content-Type": "application/json"
+                    },
+                    validateStatus: status => {
+                        return status >= 200 && status <= 505;
+                    }
+                })
+
+            if (res.data.statusCode !== 1) throw new AdvancedError(res.data.message, res.data.statusCode);
+            return {
+                ...res.data,
+                success: true
+            }
+
+        } catch (err) {
+            return {
+                success: false,
+                message: err.message,
+                statusCode: err.statusCode
+            }
+        }
+    },
+
 }
 
 
