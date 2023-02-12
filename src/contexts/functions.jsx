1f8cc64277a5d310fdb4cb1e3b8e270aaa4f6bd1
Olawande Akinmosin <samueldacoal@gmail.com>
2023-02-12 17:17:15 +0100

    updates to quiz

--- src/contexts/functions.jsx
+++ src/contexts/functions.jsx
@@ -5064,6 +5064,72 @@ export const consoleFunctions = {
             }
         }
     },
+    gradeQuestion: async function (token, id, data) {
+        try {
+            const res = await axios.post(`${baseURL}/classes/contents/quiz/question/grade/${id}`, JSON.stringify(data),
+                {
+                    headers: {
+                        "Authorization": `Bearer ${token}`,
+                        "Content-Type": "application/json"
+                    },
+                    validateStatus: status => {
+                        return status >= 200 && status <= 505;
+                    }
+                })
+
+
+            if (res.data.statusCode !== 1) throw new AdvancedError(res.data.message, res.data.statusCode);
+            return {
+                ...res.data,
+                success: true
+            }
+
+        } catch (err) {
+            if (err.statusCode === 2) {
+                localStorage.clear()
+            } else {
+
+                return {
+                    success: false,
+                    message: err.message,
+                    statusCode: err.statusCode
+                }
+            }
+        }
+    },
+    calculateGrade: async function (token, id, data) {
+        try {
+            const res = await axios.post(`${baseURL}//classes/contents/quiz/score/calculate/${id}`, JSON.stringify(data),
+                {
+                    headers: {
+                        "Authorization": `Bearer ${token}`,
+                        "Content-Type": "application/json"
+                    },
+                    validateStatus: status => {
+                        return status >= 200 && status <= 505;
+                    }
+                })
+
+
+            if (res.data.statusCode !== 1) throw new AdvancedError(res.data.message, res.data.statusCode);
+            return {
+                ...res.data,
+                success: true
+            }
+
+        } catch (err) {
+            if (err.statusCode === 2) {
+                localStorage.clear()
+            } else {
+
+                return {
+                    success: false,
+                    message: err.message,
+                    statusCode: err.statusCode
+                }
+            }
+        }
+    },
     addFile: async function (token, data) {
         try {
             const res = await axios.post(`${baseURL}/classes/content/file/add`, JSON.stringify(data),
