2326f941faef70250071448e595e5828ddada7a6
Mayowa Awoyomi <mayowaawoyomi@gmail.com>
2023-03-02 07:24:55 +0100

    fixed upcoming

--- src/components/NewLanding/UpNext.jsx
+++ src/components/NewLanding/UpNext.jsx
@@ -228,12 +228,12 @@ export function TabsComp() {
         // const first = res.data?.length > 0 ? res.data?.filter(item => item.startDate === "2023-01-19T00:00:00.000Z" && item.isActive) : [];
         // const second = res.data?.length > 0 ? res.data?.filter(item => item.startDate.includes("2023-01") && !item.startDate.includes("2023-01-19T00:00:00.000Z") && item.isActive).sort((a, b) => new Date(a.startDate) - new Date(b.startDate)) : [];
 
-        const first = res.data?.length > 0 ? res.data?.filter(item => item.startDate.includes("2023-01") && item.isActive).sort((a, b) => new Date(a.startDate) - new Date(b.startDate)) : [];
-        const second = res.data?.length > 0 ? res.data?.filter(item => item.startDate.includes("2023-02") && item.isActive).sort((a, b) => new Date(a.startDate) - new Date(b.startDate)) : [];
+        const first = res.data?.length > 0 ? res.data?.filter(item => item.startDate.includes("2023-03") && item.isActive).sort((a, b) => new Date(a.startDate) - new Date(b.startDate)) : [];
+        // const second = res.data?.length > 0 ? res.data?.filter(item => item.startDate.includes("2023-02") && item.isActive).sort((a, b) => new Date(a.startDate) - new Date(b.startDate)) : [];
 
         // const second = res.data?.length > 0 ? res.data?.filter(item => item.startDate === "2023-01-05T00:00:00.000Z" && item.isActive) : [];
         // const third = res.data?.length > 0 ? res.data?.filter(item => item.startDate !== "2023-01-05T00:00:00.000Z" && item.startDate !== "2023-01-19T00:00:00.000Z" && item.isActive).sort((a, b) => new Date(a.startDate) - new Date(b.startDate)) : [];
-        const all = [...first, ...second];
+        const all = [...first];
 
         // const uppers = res.data.filter(item => item.startDate === "2023-01-05T00:00:00.000Z" && item.isActive);
         // console.log({ uppers });
