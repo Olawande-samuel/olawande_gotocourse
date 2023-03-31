f368f46abb219fb51313a816eb64fcc28daac7ea
Olawande Akinmosin <samueldacoal@gmail.com>
2023-03-31 13:44:11 +0100

    updates

--- src/pages/Dashboard/components/classConsole/Quiz.js
+++ src/pages/Dashboard/components/classConsole/Quiz.js
@@ -433,15 +433,14 @@ export default function Quiz() {
                                                             </Select>
 
                                                             <div className="texteditor quiz__editor">  
-                                                            
-                                                             <ReactQuill theme="snow" value={x?.title ?? ""} 
-                                                                onChange={(e)=>{
-                                                                    if(formData?.questions[id]['title'] !== ""){ 
-                                                                        const list = {...formData}
-                                                                        list.questions[id]['title'] = e;
-                                                                        setFormData(list)    
-                                                                    }
-                                                                }} 
+                                                             <ReactQuill theme="snow" value={"hello there"} 
+                                                                // onChange={(e)=>{
+                                                                //     if(formData?.questions[id]['title'] !== ""){ 
+                                                                //         const list = {...formData}
+                                                                //         list.questions[id]['title'] = e;
+                                                                //         setFormData(list)    
+                                                                //     }
+                                                                // }} 
                                                             />
                 
                                                                 
